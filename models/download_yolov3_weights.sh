6874be8bc2bdb71b7b101518e9b9f92811432066
Evan Flynn <evanflynn.msu@gmail.com>
2021-08-21 21:04:05 -0700

    fix downloading weights, add some prints

---
+++ models/download_yolov3_weights.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+fname="yolov3.weights"
+# get weights
+if [ ! -f "./$fname" ]; then
+  echo "$fname doesnt exist, downloading now..."
+  wget https://pjreddie.com/media/files/$fname
+else
+  echo "$fname exists, skipping downloading"
+fi
